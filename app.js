f14b3fa9d8f2bae3ade01027a5897d3195bdc75f
vikas-suryawanshi <vikassuryawanshi395@gmail.com>
2026-05-06 10:33:45 +0530

    apply wrapAsync fnction on all async functions

--- app.js
+++ app.js
@@ -52,59 +52,60 @@ app.get("/testlisting",async(req,res)=>{
 
 // index route
 
-app.get("/listings",async (req,res)=>{
+app.get("/listings",wrapAsync(async (req,res)=>{
     let allListings=await Listing.find({});
     res.render("listings/index.ejs",{allListings});
-})
+}));
+
+// show routes
+
+app.get("/listings/:id",wrapAsync(async(req,res)=>{
+    let {id}=req.params;
+    const listings=await Listing.findById(id);
+    res.render("listings/show.ejs",{listings});
+}))
 
 // new route
 app.get("/listings/new",(req,res)=>{
     res.render("listings/new.ejs");
 });
-// show routes
-
-app.get("/listings/:id",async(req,res)=>{
-    let {id}=req.params;
-    const listings=await Listing.findById(id);
-    res.render("listings/show.ejs",{listings});
-})
-
 
 // create routes
-app.post("/listings",async (req,res)=>{
+app.post("/listings",wrapAsync(async (req,res)=>{
     const newListing=new Listing(req.body.listing);
     await newListing.save();
     res.redirect("/listings");
-})
+}));
 
 // edit route
-app.get("/listings/:id/new",async(req,res)=>{
+app.get("/listings/:id/new",wrapAsync(async(req,res)=>{
     let {id}=req.params;
     const listing=await Listing.findById(id);
     res.render("listings/edit.ejs",{listing});
-})
+}));
 
 // update routes
-app.put("/listings/:id",async(req,res)=>{
+app.put("/listings/:id",wrapAsync(async(req,res)=>{
     let {id}=req.params;
     await Listing.findByIdAndUpdate(id,(req.body.listing));
     res.redirect(`/listings`);
 
-})
+}));
 
 // delete route
-app.delete("/listings/:id",async(req,res)=>{
+app.delete("/listings/:id",wrapAsync(async(req,res)=>{
     let {id}=req.params;
     const deleteLsiting=await Listing.findByIdAndDelete(id);
     console.log(deleteLsiting);
     res.redirect("/listings");
-})
+}));
 
 
 // basic route to test working app
 app.get("/",(req,res)=>{
     res.send("app is working");
 })
+
 // for listning app
 app.listen(port,()=>{
     console.log("port is listeing on port is 8080");
