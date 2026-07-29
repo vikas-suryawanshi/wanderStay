33c1f20e8e54285c91ddc86a676d8b46c0ec7898
vikas-suryawanshi <vikassuryawanshi395@gmail.com>
2026-07-29 17:43:50 +0530

    add reviews as a refrance in the listing schema

--- models/listing.js
+++ models/listing.js
@@ -27,6 +27,10 @@ const listingSchema=new Schema({
     country:{
         type:String,
     },
+    reviews:[{
+        type:Schema.Types.ObjectId,
+        ref:"Review",
+    },]
 });
 
 const Listing=mongoose.model("Listing",listingSchema);
