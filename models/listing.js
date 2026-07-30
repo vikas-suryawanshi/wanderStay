95184e0bff354f55a032eff67c8af4b7f7a0363a
vikas-suryawanshi <vikassuryawanshi395@gmail.com>
2026-07-30 09:17:43 +0530

    add post mongoose middllewares in listing.js

--- models/listing.js
+++ models/listing.js
@@ -1,4 +1,5 @@
 const mongoose=require("mongoose");
+const Review = require("./review");
 
 const Schema=mongoose.Schema;
 
@@ -31,6 +32,12 @@ const listingSchema=new Schema({
         type:Schema.Types.ObjectId,
         ref:"Review",
     },]
+});
+
+listingSchema.post("findOneAndDelete",async(listing)=>{
+    if(listing){
+        await Review.deleteMany({_id:{$in: listing.reviews}});
+    }
 });
 
 const Listing=mongoose.model("Listing",listingSchema);
