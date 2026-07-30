d01f751ea14507fd4f8638b01cc8fe64bbed2a82
vikas-suryawanshi <vikassuryawanshi395@gmail.com>
2026-07-30 08:08:33 +0530

    solve the listingSchemas.js

--- schemas/listingSchemas.js
+++ schemas/listingSchemas.js
@@ -12,8 +12,8 @@ module.exports.listingSchema=Joi.object({
 });
 
 module.exports.reviewSchema=Joi.object({
-    review:joi.object({
-        rating:joi.Number().required(),
-        Comment:joi.String().required(),
+    review:Joi.object({
+        rating:Joi.number().required(),
+        comment:Joi.string().required(),
     }).required(),
 })
