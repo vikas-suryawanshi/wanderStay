6b8a3b61f94cc62ca5c31076d902cd14be424637
vikas-suryawanshi <vikassuryawanshi395@gmail.com>
2026-08-10 08:48:18 +0530

    create a userSchema

---
+++ models/user.js
@@ -0,0 +1,12 @@
+const { required } = require("joi");
+const mongoose=require("mongoose");
+
+const Schema=mongoose.Schema;
+
+const userSchema = new Schema({
+    email:{
+        type:String,
+        required:true,
+    },
+})
+
