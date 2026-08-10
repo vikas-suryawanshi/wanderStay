d692de37e59f61d26844ce5133d541411bbcfab3
vikas-suryawanshi <vikassuryawanshi395@gmail.com>
2026-08-10 09:25:11 +0530

    add GET route for signup page

--- routes/user.js
+++ routes/user.js
@@ -1,2 +1,9 @@
 const express = require("express");
 const router = express.Router();
+const User = require("../models/user.js");
+
+router.get("/signup",(req,res)=>{
+    res.render("users/signup.ejs");
+})
+
+module.exports=router;
