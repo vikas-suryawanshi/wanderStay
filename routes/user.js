76272a4b5242d5bf0bde368431d5302a9ad774c1
vikas-suryawanshi <vikassuryawanshi395@gmail.com>
2026-08-12 08:34:18 +0530

    require middleware saveRedirectUrlin users.js and apply login post route

--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,10 @@ const router = express.Router();
 const User = require("../models/user.js");
 const wrapAsync = require("../utils/wrapAsync.js");
 const passport = require("passport");
+const {saveRedirectUrl} = require("../middleware.js");
+
+
+
 router.get("/signup",(req,res)=>{
     res.render("users/signup.ejs");
 })
@@ -31,13 +35,13 @@ router.get("/login",(req,res)=>{
     res.render("users/login.ejs");
 })
 
-router.post("/login",passport.authenticate("local",{failureRedirect:"/login"
+router.post("/login",saveRedirectUrl,passport.authenticate("local",{failureRedirect:"/login"
     ,failureFlash:true}),
     async(req,res)=>{
         req.flash("success", "welcome back to wanderStay");
         res.redirect("/listings");
     }
-);    
+);
 
 
 router.get("/logout",async(req,res,next)=>{
