cef29302ddb34a29a57432d336b7c4f8f498c76f
vikas-suryawanshi <vikassuryawanshi395@gmail.com>
2026-08-13 07:52:15 +0530

    write review validation middleware

--- middleware.js
+++ middleware.js
@@ -39,3 +39,14 @@ module.exports.validateListing=(req,res,next)=>{
         next();
     }
 }
+
+// validate review middleware
+module.exports.reviewListing=(req,res,next)=>{
+    let {error}=reviewSchema.validate(req.body);
+    if(error){
+        let errMsg=error.details.map((el)=>el.message).join(",");
+        throw new ExpressError(400,errMsg);
+    }else{
+        next();
+    }
+}
