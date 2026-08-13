cebc8ab0af07b3259b1184d74085bba4c2aebbd2
vikas-suryawanshi <vikassuryawanshi395@gmail.com>
2026-08-13 07:49:03 +0530

    reuire validateListing middleware in listing.js

--- routes/listing.js
+++ routes/listing.js
@@ -5,25 +5,10 @@ const wrapAsync=require("../utils/wrapAsync.js");
 const ExpressError=require("../utils/ExpressError.js");
 const {listingSchema,reviewSchema}=require("../schemas/listingSchemas.js");
 const Listing = require("../models/listing");
-const {isLoggedIn ,isOwner} = require("../middleware.js");
-
-
-
-// validate listing fn
-const validateListing=(req,res,next)=>{
-    let {error}=listingSchema.validate(req.body);
-    if(error){
-        let errMsg=error.details.map((el)=>el.message).join(",");
-        throw new ExpressError(400,errMsg);
-    }else{
-        next();
-    }
-}
-
+const {isLoggedIn ,isOwner,validateListing} = require("../middleware.js");
 
 
 // index route
-
 router.get("/",wrapAsync(async (req,res)=>{
     let allListings=await Listing.find({});
     res.render("listings/index.ejs",{allListings});
