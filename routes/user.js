a086e69e66bb63114fd6eab194e5f593bca1e13c
vikas-suryawanshi <vikassuryawanshi395@gmail.com>
2026-08-16 09:52:07 +0530

    remove users  route callbacks after moving logic to controller

--- routes/user.js
+++ routes/user.js
@@ -7,52 +7,20 @@ const {saveRedirectUrl} = require("../middleware.js");
 
 
 
-router.get("/signup",(req,res)=>{
-    res.render("users/signup.ejs");
-})
+router.get("/signup",)
 
 
-router.post("/signup",wrapAsync(async(req,res)=>{
-    try{
-        let {username,email,password}=req.body;
-        const newUser = new User({username,email});
-        let registeredUser = await User.register(newUser,password);
-        req.login(registeredUser,(err)=>{
-            if(err){
-                return next(err);
-            }
-            req.flash("success","welcome to wanderstay!");
-            res.redirect("/listings");
-        })
-    }catch(e){
-        req.flash("error",e.message);
-        res.redirect("/signup");
-    }
-}))
+router.post("/signup",wrapAsync())
 
 
-router.get("/login",(req,res)=>{
-    res.render("users/login.ejs");
-})
+router.get("/login",)
 
 router.post("/login",saveRedirectUrl,passport.authenticate("local",{failureRedirect:"/login"
     ,failureFlash:true}),
-    async(req,res)=>{
-        req.flash("success", "welcome back to wanderStay");
-        let redirectUrl = res.locals.redirectUrl || "/listings";
-        res.redirect(redirectUrl);
-    }
+    
 );
 
 
-router.get("/logout",async(req,res,next)=>{
-    req.logOut((err)=>{
-        if(err){
-            return next(err);
-        }
-        req.flash("success", "logOut succesfully");
-        res.redirect("/listings");
-    });
-});
+router.get("/logout",);
 
 module.exports=router;
