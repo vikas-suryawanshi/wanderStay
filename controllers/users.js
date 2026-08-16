3e63c69c8282d755bcfb51cb309239a25cccb5e6
vikas-suryawanshi <vikassuryawanshi395@gmail.com>
2026-08-16 09:50:47 +0530

    move user route callbacks to users  controller

--- controllers/users.js
+++ controllers/users.js
@@ -1,1 +1,43 @@
 const User = require("../models/user.js");
+
+module.exports.getSignupForm = (req,res)=>{
+    res.render("users/signup.ejs");
+};
+
+module.exports.signUp = async(req,res)=>{
+    try{
+        let {username,email,password}=req.body;
+        const newUser = new User({username,email});
+        let registeredUser = await User.register(newUser,password);
+        req.login(registeredUser,(err)=>{
+            if(err){
+                return next(err);
+            }
+            req.flash("success","welcome to wanderstay!");
+            res.redirect("/listings");
+        })
+    }catch(e){
+        req.flash("error",e.message);
+        res.redirect("/signup");
+    }
+};
+
+module.exports.getLoginForm = (req,res)=>{
+    res.render("users/login.ejs");
+};
+
+module.exports.login = async(req,res)=>{
+        req.flash("success", "welcome back to wanderStay");
+        let redirectUrl = res.locals.redirectUrl || "/listings";
+        res.redirect(redirectUrl);
+};
+
+module.exports.logOut = async(req,res,next)=>{
+    req.logOut((err)=>{
+        if(err){
+            return next(err);
+        }
+        req.flash("success", "logOut succesfully");
+        res.redirect("/listings");
+    });
+};
