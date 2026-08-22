8ea518b53b901c6e02155f0395a33f66792ee2a2
vikas-suryawanshi <vikassuryawanshi395@gmail.com>
2026-08-22 18:58:05 +0530

    center map is chnaged

--- public/js/map.js
+++ public/js/map.js
@@ -1,6 +1,6 @@
 const map = new mapboxgl.Map({
         accessToken: mapToken,
         container: 'map', // container ID
-        center: [-71.06776, 42.35816], // starting position [lng, lat]. Note that lat must be set between -90 and 90
+        center: [77.2090, 28.6139], // starting position [lng, lat]. Note that lat must be set between -90 and 90
         zoom: 9 // starting zoom
     });
