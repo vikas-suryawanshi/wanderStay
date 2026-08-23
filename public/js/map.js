f1530781db6a5f3eecd4bbebd32a83419e033934
vikas-suryawanshi <vikassuryawanshi395@gmail.com>
2026-08-23 09:53:37 +0530

    add ejs

--- public/js/map.js
+++ public/js/map.js
@@ -1,6 +1,6 @@
 const map = new mapboxgl.Map({
         accessToken: mapToken,
         container: 'map', // container ID
-        center: [77.2090, 28.6139], // starting position [lng, lat]. Note that lat must be set between -90 and 90
+        center: Listing.geometry.coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
         zoom: 9 // starting zoom
     });
