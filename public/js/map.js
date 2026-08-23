94855be803f18ec9bb5944cd29a476c9580206c0
vikas-suryawanshi <vikassuryawanshi395@gmail.com>
2026-08-23 10:10:35 +0530

    display listing location on Mapbox map

--- public/js/map.js
+++ public/js/map.js
@@ -1,6 +1,15 @@
 const map = new mapboxgl.Map({
         accessToken: mapToken,
         container: 'map', // container ID
-        center: Listing.geometry.coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
+        center: listing.geometry.coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
         zoom: 9 // starting zoom
     });
+
+
+    const marker = new mapboxgl.Marker({color:"red"})
+        .setLngLat(listing.geometry.coordinates)
+        .setPopup(new mapboxgl.Popup({offset: 25})
+        .setHTML(
+         `<h4>${listing.location}</h4> <p>Exact location will be provided after booking!</p>`
+        ))
+        .addTo(map);
