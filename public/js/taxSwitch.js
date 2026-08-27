6cd016410a4e347f8f3814e51315d4db7cc7fb89
vikas-suryawanshi <vikassuryawanshi395@gmail.com>
2026-08-27 08:56:46 +0530

    remove taxswitch logic in index page and writing in js folder inside taxSwitch.js file

---
+++ public/js/taxSwitch.js
@@ -0,0 +1,11 @@
+    let taxSwitch=document.getElementById("switchCheckDefault");
+    taxSwitch.addEventListener("click",()=>{
+        let taxInfo =document.getElementsByClassName("tax-info");
+        for(let info of taxInfo){
+            if(info.style.display != "inline"){
+                info.style.display = "inline";
+            }else{
+                info.style.display = "none";
+            }
+        }
+    })
