40d2b0448229f9ddfc782ddd1cead3449e8cd255
vikas-suryawanshi <vikassuryawanshi395@gmail.com>
2026-08-31 07:28:29 +0530

    add price in taxswitch.js

--- public/js/taxSwitch.js
+++ public/js/taxSwitch.js
@@ -2,6 +2,7 @@
     taxSwitch.addEventListener("change",()=>{
         let taxInfo =document.getElementsByClassName("tax-info");
         for(let info of taxInfo){
+            let price = "<%= listings.price%>";
             if(taxSwitch == "checked"){
                 // let finalPrice = price * 1.18;
                 
