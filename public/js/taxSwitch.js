dd754af74f570fc1dcbbe6b91c75d181241973dc
vikas-suryawanshi <vikassuryawanshi395@gmail.com>
2026-08-31 07:27:28 +0530

    add taxswitch condn for check tax switch check or not

--- public/js/taxSwitch.js
+++ public/js/taxSwitch.js
@@ -1,11 +1,10 @@
     let taxSwitch=document.getElementById("switchCheckDefault");
-    taxSwitch.addEventListener("click",()=>{
+    taxSwitch.addEventListener("change",()=>{
         let taxInfo =document.getElementsByClassName("tax-info");
         for(let info of taxInfo){
-            if(info.style.display != "inline"){
-                info.style.display = "inline";
-            }else{
-                info.style.display = "none";
+            if(taxSwitch == "checked"){
+                // let finalPrice = price * 1.18;
+                
             }
         }
     })
