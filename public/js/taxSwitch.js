d9c7641104b361ca14d5fc802bf98cc9cf746bf5
vikas-suryawanshi <vikassuryawanshi395@gmail.com>
2026-08-31 07:31:36 +0530

    add tax info is an addGst

--- public/js/taxSwitch.js
+++ public/js/taxSwitch.js
@@ -1,11 +1,9 @@
     let taxSwitch=document.getElementById("switchCheckDefault");
     taxSwitch.addEventListener("change",()=>{
         let taxInfo =document.getElementsByClassName("tax-info");
-        for(let info of taxInfo){
-            let price = "<%= listings.price%>";
+        for(let addGst of taxInfo){
+            let price = Number(addGst.dataset.price);
             if(taxSwitch == "checked"){
-                let finalPrice = price * 1.18;
-                
             }
         }
     })
