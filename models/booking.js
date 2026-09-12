a1a7567e8110002cca31898cf28eeea92199b10b
vikas-suryawanshi <vikassuryawanshi395@gmail.com>
2026-09-12 18:13:23 +0530

    add user filed in bookingSchema

--- models/booking.js
+++ models/booking.js
@@ -1,7 +1,10 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-
-const bookingSchema = new Schema {
-
-}
+const bookingSchema = new Schema({
+    user:{
+        type:Schema.Types.ObjectId,
+        ref:"User",
+        required:true,
+    },
+});
