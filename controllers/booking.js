8b11eed6db30de994d8ab21bace69aa20b164f1f
vikas-suryawanshi <vikassuryawanshi395@gmail.com>
2026-09-13 21:07:59 +0530

    validate booking request data

--- controllers/booking.js
+++ controllers/booking.js
@@ -7,4 +7,11 @@ module.exports.createBooking = async(req,res)=>{
         req.flash("error","this listing is not avalble");
         return res.redirect("/listings");
     }
+
+    let {checkIn,checkOut,guests} = req.body;
+    if(!checkIn || !checkOut || !guests){
+        req.flash("error","please all field fill correctly & try again.");
+        return res.redirect(`/listings/${req.params.id}`);
+    }
+    let user = req.user._id;
 }
