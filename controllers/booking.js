782b6deaefd766d318139abf7d990a72e8fdabc4
vikas-suryawanshi <vikassuryawanshi395@gmail.com>
2026-09-13 20:54:47 +0530

    handle invalid listing in booking controller

--- controllers/booking.js
+++ controllers/booking.js
@@ -1,3 +1,10 @@
-module.exports.createBooking = async(req,res)=>{
+const Listing = require("../models/listing");
+const Listing = require("../models/listing");
 
+module.exports.createBooking = async(req,res)=>{
+    let listing = await Listing.findById(req.params.id);
+    if(!listing){
+        req.flash("error","this listing is not avalble");
+        return res.redirect("/listings");
+    }
 }
