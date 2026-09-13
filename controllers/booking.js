ab3adee5640384e768e78a7cd65a93b4f04b2f1f
vikas-suryawanshi <vikassuryawanshi395@gmail.com>
2026-09-13 22:13:59 +0530

    create and save booking

--- controllers/booking.js
+++ controllers/booking.js
@@ -1,5 +1,6 @@
 const Listing = require("../models/listing");
 const Booking = require("../models/booking");
+const { newListing } = require("./listing");
 
 module.exports.createBooking = async(req,res)=>{
     let listing = await Listing.findById(req.params.id);
@@ -35,4 +36,17 @@ module.exports.createBooking = async(req,res)=>{
     }
     let numberOfNights = (checkOut-checkIn)/(24*60*60*1000);
     let totalPrice = listing.price * numberOfNights;
+
+    let booking = new Booking({
+        user:user,
+        listing:listing,
+        checkIn:checkIn,
+        checkOut:checkOut,
+        guests: guests,
+        totalPrice: totalPrice
+
+    })
+    await booking.save();
+    req.flash("success","you are succesfully resever this listing");
+    return res.redirect(`/listings/${req.params.id}`);
 }
