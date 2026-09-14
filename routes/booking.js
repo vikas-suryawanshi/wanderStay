c80430ae9596aca98232d2c2625e0f34abc5db94
vikas-suryawanshi <vikassuryawanshi395@gmail.com>
2026-09-14 21:47:17 +0530

    get routes are succesfully implemented

--- routes/booking.js
+++ routes/booking.js
@@ -4,7 +4,7 @@ const wrapAsync=require("../utils/wrapAsync.js");
 const bookingController = require("../controllers/booking.js");
 
 router.route("/listings/:id/book")
-.get()
+.get(bookingController.getBookingForm)
 .post(wrapAsync(bookingController.createBooking));
 
 module.exports=router;
