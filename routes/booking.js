2b15e9b2c9860ebcfe96de17404adb9b7ec996c8
vikas-suryawanshi <vikassuryawanshi395@gmail.com>
2026-09-14 21:34:27 +0530

    add get route in router folder

--- routes/booking.js
+++ routes/booking.js
@@ -4,6 +4,9 @@ const wrapAsync=require("../utils/wrapAsync.js");
 const bookingController = require("../controllers/booking.js");
 
 router.route("/listings/:id/book")
+.get(wrapAsync,async(req,res)=>{
+    res.render("booking.ejs");
+})
 .post(wrapAsync(bookingController.createBooking));
 
 module.exports=router;
